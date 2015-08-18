427399fe8a0470651cf441510ca6468b3a2f31f1
Courtney Mitchell <courtney.mitchell@live.com>
2015-08-18 10:36:20 -0700

    scaffold steps. add new goal

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -31,7 +31,7 @@ class GoalsController < ApplicationController
     respond_to do |format|
       if @goal.save
         format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
-        format.json { render :show, status: :created, location: @goal }
+        format.json { render json: @goal, root:false, status: :created }
       else
         format.html { render :new }
         format.json { render json: @goal.errors, status: :unprocessable_entity }
